8589caa158f16ddcc474e4bdaabca0aa80cbe1ee
Mohammad Fatemi <mr.smf8@gmail.com>
2021-07-16 17:56:09 +0430

    feat: Implement category repo

--- internal/app/gokkan/model/category.go
+++ internal/app/gokkan/model/category.go
@@ -1,7 +1,42 @@
 package model
 
+import "gorm.io/gorm"
+
 // Category specifies an item category
 type Category struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
+}
+
+// CategoryRepo defines allowed operations on Category
+type CategoryRepo interface {
+	Delete(name string) error
+	Save(category *Category) error
+	FindAll() ([]Category, error)
+}
+
+// make sure SQLCategoryRepo implements Category at compile time
+var _ CategoryRepo = SQLCategoryRepo{}
+
+// SQLCategoryRepo is SQL implementation of CategoryRepo
+type SQLCategoryRepo struct {
+	DB *gorm.DB
+}
+
+// Delete removes a category with given name.
+func (c SQLCategoryRepo) Delete(name string) error {
+	return c.DB.Where("name = ?", name).Delete(&Category{}).Error
+}
+
+// Save saves given category. if it contains an ID it's updated
+func (c SQLCategoryRepo) Save(category *Category) error {
+	return c.DB.Save(category).Error
+}
+
+// FindAll retrieves all categories.
+func (c SQLCategoryRepo) FindAll() ([]Category, error) {
+	var result []Category
+	err := c.DB.Find(&result).Error
+
+	return result, err
 }
