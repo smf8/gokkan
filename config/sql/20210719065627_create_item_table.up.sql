b9a08f6bebc553507b2560a0f8ff677eb5c5effa
Mohammad Fatemi <mr.smf8@gmail.com>
2021-07-19 13:16:53 +0430

    fix: Fix items table migration

--- config/sql/20210719065627_create_item_table.up.sql
+++ config/sql/20210719065627_create_item_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS items(
     id BIGSERIAL PRIMARY KEY,
     category_id BIGSERIAL DEFAULT 1,
-    price FLOAT(8),
-    remaining INTEGER,
-    sold INTEGER,
+    price FLOAT(8) NOT NULL,
+    remaining INTEGER NOT NULL,
+    sold INTEGER DEFAULT 0,
     photo_url varchar(255),
-    created_at      timestamp not null default now(),
+    created_at      timestamp NOT NULL DEFAULT now(),
     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET DEFAULT
  );
