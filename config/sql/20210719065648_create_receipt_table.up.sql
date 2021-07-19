8254a1eb174fb4c9dd8887d940e33b0c53517bf6
Mohammad Fatemi <mr.smf8@gmail.com>
2021-07-19 13:24:47 +0430

    feat: Add receipts table migration

---
+++ config/sql/20210719065648_create_receipt_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS receipts(
+    id BIGSERIAL PRIMARY KEY,
+    item_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    quantity INTEGER NOT NULL,
+    buyer_name VARCHAR(255) NOT NULL,
+    billing_address VARCHAR(255) NOT NULL,
+    price FLOAT(8) NOT NULL,
+    date TIMESTAMP NOT NULL  DEFAULT now(),
+    tracking_code SERIAL NOT NULL,
+    status INTEGER NOT NULL,
+    CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (id) ON DELETE NO ACTION,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION
+);
