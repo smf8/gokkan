dfede7055e11fac8e2e58ff09c8bfe5dff08bf05
Mohammad Fatemi <mr.smf8@gmail.com>
2021-07-19 13:20:18 +0430

    fix: User big int for foreign key in items table

--- config/sql/20210719065627_create_item_table.up.sql
+++ config/sql/20210719065627_create_item_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS items(
     id BIGSERIAL PRIMARY KEY,
-    category_id BIGSERIAL DEFAULT 1,
+    category_id BIGINT DEFAULT 1,
     price FLOAT(8) NOT NULL,
     remaining INTEGER NOT NULL,
     sold INTEGER DEFAULT 0,
