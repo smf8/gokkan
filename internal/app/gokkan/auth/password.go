964243388d412976b4f8a9f3dcad32b4b8361f56
Mohammad Fatemi <mr.smf8@gmail.com>
2021-07-20 19:22:51 +0430

    feat: Add password hashing functions

---
+++ internal/app/gokkan/auth/password.go
@@ -0,0 +1,17 @@
+package auth
+
+import "crypto/sha256"
+
+// Hash function generates hash with SHA256 algorithm.
+func Hash(password string) string {
+	hash := sha256.Sum256([]byte(password))
+
+	return string(hash[:])
+}
+
+// CheckPassword checks password and hash to see if they are equal.
+func CheckPassword(password, hash string) bool {
+	passwordHash := sha256.Sum256([]byte(password))
+
+	return string(passwordHash[:]) == hash
+}
